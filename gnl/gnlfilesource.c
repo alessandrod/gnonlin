d779fabe63d22274f096f0cf07154796f71aecd0
Edward Hervey <bilboed@bilboed.com>
2009-04-16 15:18:14 +0200

    gnlfilesource: Make decodebin2 the default.

--- gnl/gnlfilesource.c
+++ gnl/gnlfilesource.c
@@ -131,11 +131,10 @@ gnl_filesource_init (GnlFileSource * filesource,
             gst_element_factory_make ("filesrc", "internal-filesource")))
       g_warning
           ("Could not create a gnomevfssrc or filesource element, are you sure you have any of them installed ?");
-  if (g_getenv ("USE_DECODEBIN2"))
-    decodebin = gst_element_factory_make ("decodebin2", "internal-decodebin");
-  else
-    decodebin = gst_element_factory_make ("decodebin", "internal-decodebin");
-  if (!decodebin)
+  if (!(decodebin =
+          gst_element_factory_make ("decodebin2", "internal-decodebin"))
+      || !(decodebin =
+          gst_element_factory_make ("decodebin", "internal-decodebin")))
     g_warning
         ("Could not create a decodebin element, are you sure you have decodebin installed ?");
 
